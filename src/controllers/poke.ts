04fc256ab2d3adb25416d8585aeb6bac5bc28945
Herless Alvarado <herlessalvarado@gmail.com>
2023-10-04 20:12:02 -0500

    logger

--- src/controllers/poke.ts
+++ src/controllers/poke.ts
@@ -1,11 +1,15 @@
 import type { Request, Response } from 'express';
+import { logger } from 'src/logger';
 import axios from 'axios';
 
 class PokeController {
   static async searchByName(_req: Request, res: Response): Promise<void> {
     const name = _req.query.name;
+    logger.info(`Searching for ${name}`);
     const { data } = await axios.get(`${process.env.POKE_API_URL}/get/${name}}`);
+    logger.info(`Found ${data.name}`);
     const images = await axios.get(`${process.env.POKE_IMAGES_URL}/pokemon/images/${data.id}`);
+    logger.info(`Found ${images.data.length} images`);
     res.status(200).send({ ...data, images: images.data });
   }
 }
